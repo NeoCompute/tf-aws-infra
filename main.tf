786aef8f64da9602e790f3a178021ff305f03b61
Akhilesh S Kumbar <akhilkumbar@outlook.com>
2024-10-07 20:43:20 -0400

    IaC configured

---
+++ main.tf
@@ -0,0 +1,80 @@
+provider "aws" {
+  region  = var.region
+  profile = var.aws_profile
+}
+
+# VPC
+resource "aws_vpc" "vpc-01" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "vpc-01"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "ig-01" {
+  vpc_id = aws_vpc.vpc-01.id
+  tags = {
+    Name = "ig-01"
+  }
+}
+
+# Public Subnets in 3 Availability Zones
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.vpc-01.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+# Private Subnets in 3 Availability Zones
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.vpc-01.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc-01.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# A route in the Public Route Table that sends traffic to the Internet Gateway
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ig-01.id
+}
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_associations" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Private Route Table (no internet route)
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc-01.id
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+# Associate Private Subnets with Private Route Table
+resource "aws_route_table_association" "private_associations" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
